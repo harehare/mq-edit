61a672785e4edcf7f0b1a3b5d367f3f56e3eeccd
harehare <harehare1110@gmail.com>
2026-04-10 21:31:30 +0900

    🔧 Update installation path to use ~/.local/bin for binaries

--- bin/install.sh
+++ bin/install.sh
@@ -5,8 +5,7 @@ set -e
 # mq-edit installation script
 
 readonly MQ_REPO="harehare/mq-edit"
-readonly MQ_INSTALL_DIR="$HOME/.mq"
-readonly MQ_BIN_DIR="$MQ_INSTALL_DIR/bin"
+readonly MQ_BIN_DIR="$HOME/.local/bin"
 
 
 # Colors for output
